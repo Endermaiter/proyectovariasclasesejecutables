f2e611e1bd0ab62465632aefaa3f0fe9f28cf3e3
Endermaiter <mpereznovoa@gmail.com>
2021-10-13 21:56:01 +0200

    Commit del readme.md

--- src/com/dam1/Paquete1/Main.java
+++ src/com/dam1/Paquete1/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Nuevo");
         System.out.println("Modificacion1");
+        System.out.println("Modificacion2");
     }
 }
--- src/com/dam1/Paquete2/Main.java
+++ src/com/dam1/Paquete2/Main.java
@@ -1,10 +1,9 @@
 package com.dam1.Paquete2;
 
 public class Main {
-
     public static void main(String[] args) {
 	System.out.println("Ejercicio1");
     System.out.println("Modificación1");
-
+    System.out.println("Modificacion2");
     }
 }

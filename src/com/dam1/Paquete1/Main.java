b954dfea9db04131fa111befe7d487022af0603c
Endermaiter <mpereznovoa@gmail.com>
2021-10-13 21:55:26 +0200

    Commit del readme.md

--- src/com/dam1/Paquete1/Main.java
+++ src/com/dam1/Paquete1/Main.java
@@ -3,5 +3,6 @@ package com.dam1.Paquete1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Nuevo");
+        System.out.println("Modificacion1");
     }
 }

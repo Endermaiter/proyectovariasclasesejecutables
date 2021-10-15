a96ce92c7f875b9d67b24037ae1533e32ead6cd1
Endermaiter <mpereznovoa@danielcastelao.org>
2021-10-15 09:30:23 +0200

    Comit de clase 15/10/21

--- src/com/dam1/ejercicio2/Main.java
+++ src/com/dam1/ejercicio2/Main.java
@@ -3,5 +3,6 @@ package com.dam1.ejercicio2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Nuevo1");
+        System.out.println("Nuevo1");
     }
 }
